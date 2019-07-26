d68116ab2ef6269a6deb07579975ae2358a12a6c
Jose Ari Junior <joseari.jr@gmail.com>
2019-07-26 10:53:04 -0400

    Aula 8

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,22 @@
+//Comentário em Go
+package main
+
+/*
+Os codigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// A porta de entrada de um programa em go é a função main
+func main() {
+	fmt.Print("primero ")
+	fmt.Print("programa")
+
+	/*
+		Sobre comentários...
+
+		1) Priorize código legível e faça comentários que agrega valor!
+		2) Evite comentários óbvios
+		3) Durante o curso abuse dos comentários
+	*/
+}
