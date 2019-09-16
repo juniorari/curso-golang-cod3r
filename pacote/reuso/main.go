f60e5d54ad7f793569fa179401d2ccc73addfdaf
Jose Ari Junior <joseari.jr@gmail.com>
2019-09-16 15:41:47 -0400

    Aulas 63 a 66

--- pacote/reuso/main.go
+++ pacote/reuso/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
-	"github.com/juniorari/area"
+	"github.com/juniorari/goarea"
 )
 
+// Necessário rodar o comando:
+/*
+$ go get -u github.com/juniorari/goarea
+*/
 func main() {
-	fmt.Println(area.Circ(6.0))
-	fmt.Println(area.Rect(5.0, 2.0))
+	fmt.Println(goarea.Circ(6.0))
+	fmt.Println(goarea.Rect(5.0, 2.0))
 	// fmt.Println(area._TrianguloEq(5.0, 2.0))
 }
